57b231df3598414c4155991e23b6683e86ee74c8
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-02-07 11:23:07 +0000

    fixed scss loader

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Helmet from "react-helmet";
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
