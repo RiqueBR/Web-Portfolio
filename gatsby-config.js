5859481edd3498ff76f3537fdb51b4f5c98c3e56
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-02-07 10:41:51 +0000

    tweaking title of website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Web Portfolio`,
+    description: `A practice lesson on Gatsby, React and GraphQL `,
+    author: `Henrique Batista`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
