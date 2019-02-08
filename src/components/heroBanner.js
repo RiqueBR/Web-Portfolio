0875f42e9fccf88174deb86fafb4401a91f7de43
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-02-08 13:30:44 +0000

    seeding file added

---
+++ src/components/heroBanner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HeroBanner extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Hello, My name is Henrique</p>
+            </div>
+        );
+    }
+}
+
+export default HeroBanner;
